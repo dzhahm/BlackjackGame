f6f18085d253a126e12d713475ce346aa305c0b4
Dzhihan Ahmedov <DzhihanIlmaz.Ahmedov@varian.com>
2021-05-12 21:38:44 +0200

    PlayerViewModel is added. PlayerCardsViewModel is integrated int PlayerViewModel

---
+++ BlackjackGame/BlackjackGameUI/View/PlayerView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BlackjackGameUI.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BlackjackGameUI.View
+{
+  /// <summary>
+  /// Interaction logic for PlayerView.xaml
+  /// </summary>
+  public partial class PlayerView : UserControl
+  {
+    public PlayerView()
+    {
+      InitializeComponent();
+      DataContext = new PlayerViewModel((PlayerCardsViewModel) CardsView.DataContext);
+    }
+
+    private void HitButton_OnClick(object sender, RoutedEventArgs e)
+    {
+      ((PlayerViewModel) DataContext).OnHitCall();
+    }
+  }
+}
--- BlackjackGame/BlackjackGameUI/ViewModel/PlayerCardsViewModel.cs
+++ BlackjackGame/BlackjackGameUI/ViewModel/PlayerCardsViewModel.cs
@@ -1,9 +1,13 @@
-﻿using BlackjackGameUI.Annotations;
+﻿using BlackjackGameLibrary.Game.Round.Commands;
+using BlackjackGameLibrary.PlayingCards;
+using BlackjackGameUI.Annotations;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media.Imaging;
 
 namespace BlackjackGameUI.ViewModel
@@ -67,6 +71,92 @@ namespace BlackjackGameUI.ViewModel
     {
       _baseCardFolder = Path.Combine(Environment.CurrentDirectory, "Cards");
       InitCards();
+    }
+
+    public void UpdateCards(List<Card> cards)
+    {
+      if (cards.Count == 0)
+      {
+        ClearCardImages();
+      }
+
+      if (cards.Count > 0)
+      {
+        FirstCardToBeDisplayed = GetCardImage(cards[0]);
+      }
+
+      if (cards.Count > 1)
+      {
+        SecondCardToBeDisplayed = GetCardImage(cards[1]);
+      }
+
+      if (cards.Count > 2)
+      {
+        ThirdCardToBeDisplayed = GetCardImage(cards[2]);
+      }
+
+      if (cards.Count > 3)
+      {
+        FourthCardToBeDisplayed = GetCardImage(cards[3]);
+      }
+
+      if (cards.Count > 4)
+      {
+        //TODO
+      }
+    }
+
+
+    public void ClearCardImages()
+    {
+      FirstCardToBeDisplayed = new Image();
+      SecondCardToBeDisplayed = new Image();
+      ThirdCardToBeDisplayed = new Image();
+      FourthCardToBeDisplayed = new Image();
+    }
+
+    private Image GetCardImage(Card card)
+    {
+      string cardFirstIdentifier = null;
+      string cardSecondIdentifier = null;
+
+      switch (card.CardType)
+      {
+        case ECardType.Ace:
+          cardFirstIdentifier = "A";
+          break;
+        case ECardType.King:
+          cardFirstIdentifier = "K";
+          break;
+        case ECardType.Queen:
+          cardFirstIdentifier = "Q";
+          break;
+        case ECardType.Jack:
+          cardFirstIdentifier = "J";
+          break;
+        case ECardType.Numerical:
+          cardFirstIdentifier = card.Value.ToString();
+          break;
+      }
+
+      switch (card.CardSuit)
+      {
+        case ECardSuitTypes.Clubs:
+          cardSecondIdentifier = "C";
+          break;
+        case ECardSuitTypes.Diamonds:
+          cardSecondIdentifier = "D";
+          break;
+        case ECardSuitTypes.Hearts:
+          cardSecondIdentifier = "H";
+          break;
+        case ECardSuitTypes.Spades:
+          cardSecondIdentifier = "S";
+          break;
+      }
+
+      string cardFileName = cardFirstIdentifier + cardSecondIdentifier + ".png";
+      return new Image {Source = new BitmapImage(new Uri(Path.Combine(_baseCardFolder, cardFileName)))};
     }
 
     private void InitCards()
